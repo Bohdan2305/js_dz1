c8397d0ec79230bef32d103ac3e0c2bebc5f6907
Bohdan Khavalka <117353480+Bohdan2305@users.noreply.github.com>
2022-12-06 00:28:47 +0200

    Add files via upload

---
+++ js_1/app.js
@@ -0,0 +1,22 @@
+ 
+// Zavdannya1
+
+let city = "Київ";
+city = "Львів";
+let address = city;
+alert(address);
+
+// Zavdannya2
+
+let n = 10;
+let result = Math.pow( n,3)
+console.log(result)
+
+// Zavdannya3
+
+let r = 4;
+let o = 6;
+let vr = 5.25;
+let vo = 3.50;
+let s = r * vr + o * vo;
+document.write(s);
